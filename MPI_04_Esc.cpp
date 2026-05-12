da31af011fc41f5ea723459d012a2248c31ea6de
Yago Campello <121520061+yago-cmp@users.noreply.github.com>
2026-05-12 13:42:39 -0500

    Update MPI_04_Esc.cpp

--- MPI_04_Esc.cpp
+++ MPI_04_Esc.cpp
@@ -22,9 +22,13 @@ int main(int argc, char** argv) {
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &tam); // tamanho do cluster
+    MPI_Comm_size(MPI_COMM_WORLD, &tam); 
 
     int N = atoi(argv[1]);
+    if(N % tam != 0){
+        printf("Insira um Tamanho de vetor divisivel pelo No de Processos!!! \n");
+        return 1;
+    }
     int parte = N/tam;
 
     double result = 0;
@@ -42,11 +46,14 @@ int main(int argc, char** argv) {
             MPI_Send(&x[(i)*(parte)], parte, MPI_DOUBLE, i, tagX, MPI_COMM_WORLD);
             MPI_Send(&y[(i)*(parte)], parte, MPI_DOUBLE, i, tagY, MPI_COMM_WORLD);
         }
+
+        result += escalar(x, y, parte);
+
         for (int i = 1; i < tam; i++){    
             MPI_Recv(&recebido, 1, MPI_DOUBLE, MPI_ANY_SOURCE, tagR, MPI_COMM_WORLD, &status);
             result += recebido;
         }
-        result += escalar(x, y, parte);
+
         printf("O resultado é: %f\n", result);
     }
     else {
