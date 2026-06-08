48d47421737aa7b6916d48148d28d8fcfa109ed5
teutio <yagopcampello@gmail.com>
2026-06-08 19:59:26 -0300

    iniciando com mpi no trabalho

--- Trabalho_MPI_openMP/MPI/MPI_Par_1.cpp
+++ Trabalho_MPI_openMP/MPI/MPI_Par_1.cpp
@@ -3,16 +3,12 @@
 #include <string.h>
 #include <vector>
 #include <sys/time.h>
+#include <mpi.h>
 
 struct timeval tstart, tend;
 
 typedef unsigned int uint;
 
-/**Turn on exception reporting for a stream
- *
- * Any read or write error on stream will raise an exception -- no error
- * will be mistakenbly silenced!
- */
 template <class _STREAM> void TurnExceptionsOn(_STREAM& stream)
 {
         stream.exceptions(std::ios_base::badbit |
@@ -20,23 +16,6 @@ template <class _STREAM> void TurnExceptionsOn(_STREAM& stream)
                           std::ios_base::eofbit);
 }
 
-
-/**Make a N x N matrix.
- *
- * Care must be taken free'ing matrixes create using this function.
- * Correction: just don't delete[] this matrix. If you **REALLLY** want
- * to free this matrix you've got to proceed as follows:
- *
- * 	uint** M = makeSquareMatrix(n); 
- * 	uint* store = &M[0][0]
- * 	uint* M_rows = &M[0]; 
- * 	delete[] store;
- * 	delete[] M_rows;
- *
- * @eturn The square matrix or NULL on non-fatal errors.
- *
- * @warning May throw an exception if allocation fails.
- */
 uint** makeSquareMatrix(uint n)
 {
 	if (n == 0) {
@@ -52,59 +31,40 @@ uint** makeSquareMatrix(uint n)
 	memset(store, 0x00, n * n * sizeof(uint));
 	// Setup array B
 	B = (uint**) new uint*[n];
-	for (int row = 0; row < n; ++row) {
+	for (int row = 0; row < n; ++row) 
 		B[row] = &store[row * n];
-	}
-
 	return B;
 
 }
 
-/***Read a undirected graph from @p input
- *
- * @param input Stream from where the graph is read.
- * @param n nunber of nodes in the graph. It is returned by reference to
- * 	the callee.
- *
- * @return A incidence matrix (square matrix w/ n rows) with the graph.
- *
- * @warning Errors reading the number of nodes in the graph will throw an
- * 	exception.
- */
 uint** readFile(std::istream& input, uint& n)
 {
 	uint** M = NULL;
 	uint u, v, k;
 
-	// Read number of nodes -- MUST be in input's first line 
-	// If any error occour while we read the first line, throw an exception
 	n = 0;
 	TurnExceptionsOn(input);
+
 	input >> n;
-	// Turned Exception Reporting OFF - it was kinda silly but whatever.
 	input.exceptions(std::ios_base::goodbit);
-	// Allocate matrix
+
 	M = makeSquareMatrix(n);
 	if (M == NULL) {
 		return NULL;
 	}
-	// Read graph;
+
 	while (input >> u >> v >> k) {
 		M[u][v] = k;
 		M[v][u] = k;
 	}
-
 	return M;
 }
 
 
-/**********************************************************************
- * MAIN ALGORITHM
- **********************************************************************/
-
-
-double findClusterCoefficient(uint** M, uint n)
+double findClusterCoefficient(uint** M, uint n, int tam, int rank)
 {
+	int parte = n/tam;
+	double global_cc = 0.0;
 	double partial_cc = 0.0;
 	std::vector<uint> neighbors;
 
@@ -137,60 +97,78 @@ double findClusterCoefficient(uint** M, uint n)
 				}
 			}
 		}
-		// Remember: The graph is undireted, so M[u][v] is treated as
-		// being equivalent to M[v][u]. But in the loop above we only
-		// accounted for one of those edges (namely, M[u][v]). Thus
-		// n_triangles should be doubled.
-		// Also notice that the maximum number of clusters between
-		// i's neighbors is nei_len * (nei_len - 1)
 		partial_cc += 2 * n_triangles / double(nei_len * (nei_len - 1));
 	}
+	MPI_Reduce(&partial_cc, &global_cc, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-	return partial_cc / n;
+	if(rank == 0)
+		return partial_cc / n;
+	else
+		return 0;
 }
-
-
-
-/**********************************************************************
- * MAIN APP LOGIC
- **********************************************************************/
 
 
 int main(int argc, char* argv[])
 {
 	uint** matrix = NULL;
-	uint n;
+	uint n = 4;
 
-        std::ifstream input;
-        std::ofstream output;
+	int i, j, k;
+
+	int tagN = 0;
+	
+	int rank;
+	int tam;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &tam); 
+
+	MPI_Status status;
+
+    std::ifstream input;
+    std::ofstream output;
 
 	TurnExceptionsOn(input);
 	TurnExceptionsOn(output);
 
-	if (argc != 3) {
-                std::cerr << "ERROR: Wrong number of arguments.\n" <<
-                             "Usage: ./clustering_coefficient INPUT OUTPUT\n" 
-			     << std::endl;
-                exit(EXIT_FAILURE);
+	if(rank == 0){
+		if (argc != 3) {
+					std::cerr << "ERROR: Wrong number of arguments.\n" <<
+								"Usage: ./clustering_coefficient INPUT OUTPUT\n" 
+					<< std::endl;
+					exit(EXIT_FAILURE);
+		}
+
+		input.open(argv[1]);  
+		output.open(argv[2]);
+
+		matrix = readFile(input, n);
+		if (matrix == NULL) {
+			std::cerr << "Error creating square matrix" << std::endl;
+			return 1;
+		}
 	}
+	
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD); // envia e recebe N
 
-	input.open(argv[1]);  
-	output.open(argv[2]);
+	if(rank != 0)
+		matrix = makeSquareMatrix(n); // inicia a matriz
 
-	matrix = readFile(input, n);
-	if (matrix == NULL) {
-		std::cerr << "Error creating square matrix" << std::endl;
-		return 1;
-	}
-
-  	gettimeofday(&tstart, NULL);
+	MPI_Bcast(matrix[0], n*n, MPI_UNSIGNED, 0, MPI_COMM_WORLD); // matriz é enviada
+	
+	if(rank==0)
+  		gettimeofday(&tstart, NULL);
 //--------- Mede o tempo da função abaixo ---------------
-	float clustering_coefficient = findClusterCoefficient(matrix, n);
+	float clustering_coefficient = findClusterCoefficient(matrix, n, tam, rank);
 //-------------------------------------------------------
-	gettimeofday(&tend, NULL);
+	if(rank==0){
+		gettimeofday(&tend, NULL);
+		long tempo = (tend.tv_sec * 1000000 + tend.tv_usec)-(tstart.tv_sec * 1000000 + tstart.tv_usec);
+		output << clustering_coefficient << std::endl; // resultado na file
+		std::cout << tempo; 
+	}
 
-	long tempo = (tend.tv_sec * 1000000 + tend.tv_usec)-(tstart.tv_sec * 1000000 + tstart.tv_usec);
-
-	output << clustering_coefficient << std::endl; // resultado na file
-	std::cout << tempo; 
+	MPI_Finalize();
+	return 0;
 }
